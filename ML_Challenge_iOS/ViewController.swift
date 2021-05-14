d34f0fa9e2441f3c0da216aec0592b5be3f2cef5
Hernan Ruiz <hernan.ruiz@fluxit.com.ar>
2021-05-14 19:52:54 -0300

    Initial Commit

---
+++ ML_Challenge_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ML_Challenge_iOS
+//
+//  Created by Hernan Ruiz on 14/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
